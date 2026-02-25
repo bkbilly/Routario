2a79046d544abe8801ad1bc006018756a3b674ce
bkbilly <bkbilly@hotmail.com>
2026-02-25 23:16:30 +0200

    Allow any length of IMEI

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -111,7 +111,7 @@ class DeviceConfig(BaseModel):
 
 
 class DeviceCreate(BaseModel):
-    imei: str = Field(..., min_length=15, max_length=20)
+    imei: str = Field(..., min_length=1, max_length=20)
     name: str = Field(..., min_length=1, max_length=100)
     protocol: str = "teltonika"
     vehicle_type: Optional[str] = "car"
