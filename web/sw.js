1adf2096130a083f5e5d466def6329b1f33fc943
bkbilly <bkbilly@hotmail.com>
2026-03-01 16:00:46 +0200

    update sw

--- web/sw.js
+++ web/sw.js
@@ -4,7 +4,7 @@
  * Place this file at: /web/sw.js  (root of your web directory)
  */
 
-const CACHE_NAME = 'gps-dashboard-v7';
+const CACHE_NAME = 'gps-dashboard-v8';
 const STATIC_ASSETS = [
   '/gps-dashboard.html',
   '/device-management.html',
