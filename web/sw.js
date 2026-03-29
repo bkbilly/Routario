ac628bd6e091b6f4cad8b2aac12966404518e6ad
bkbilly <bkbilly@hotmail.com>
2026-03-29 14:08:15 +0300

    Fix backup/restore

--- web/sw.js
+++ web/sw.js
@@ -4,7 +4,7 @@
  * Place this file at: /web/sw.js  (root of your web directory)
  */
 
-const CACHE_NAME = 'gps-dashboard-v35';
+const CACHE_NAME = 'gps-dashboard-v36';
 const STATIC_ASSETS = [
   '/gps-dashboard.html',
   '/device-management.html',
